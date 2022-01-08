c4c4113481e7a6f3f69fac2dc11ac5a0e68ea6b0
Fernando Ancona <f.anconac@gmail.com>
2022-01-08 03:10:11 +0000

    add Shared in menu, heading & profile pic

--- src/app/notes/pages/home/home.component.ts
+++ src/app/notes/pages/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SocialAuthService, SocialUser } from 'angularx-social-login';
 import { AuthService } from '../../../auth/auth.service';
+
+
 
 @Component({
   selector: 'app-home',
@@ -16,10 +18,18 @@ import { AuthService } from '../../../auth/auth.service';
 export class HomeComponent implements OnInit {
 
   user!: SocialUser;
+  heading: string = 'TheNotesApp'
+
+  titles: Record<string, string> =  {
+    'home': 'My Notes',
+    'shared': 'Shared with Me',
+    'settings': 'Settings',
+  }
 
   constructor( private router: Router,
     private socialAuthService: SocialAuthService,
-    private authService: AuthService
+    private authService: AuthService,
+    private activatedRoute: ActivatedRoute,
     ) {
 
   }
@@ -34,6 +44,14 @@ export class HomeComponent implements OnInit {
 
   logout(): void {
     this.socialAuthService.signOut().then(() => this.router.navigate(['login']));
+  }
+
+  updateHeading(heading: string) {
+    // console.log(this.router.url)
+    // const endpoint = this.router.url.split('/').pop() || '';
+    // this.pageTitle = this.titles[endpoint] ? this.titles[endpoint] : this.titles['home'];
+
+    this.heading = heading;
   }
 
 }
