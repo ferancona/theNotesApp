8d8db8dfae8b3561e26a0f08da76e818fa50a5fb
Fernando Ancona <f.anconac@gmail.com>
2022-01-08 03:08:58 +0000

    editable card (title & body), updates with debouncer

--- src/app/notes/components/notes-card-editable/notes-card-editable.component.ts
+++ src/app/notes/components/notes-card-editable/notes-card-editable.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
+import { Note, createEmptyNote } from '../../interfaces/notes.interface';
+import { NotesService } from '../../services/notes.service';
 
 @Component({
   selector: 'app-notes-card-editable',
   templateUrl: './notes-card-editable.component.html',
-  styles: [
-  ]
+  styleUrls: ['./notes-card-editable.component.css']
 })
 export class NotesCardEditableComponent implements OnInit {
 
-  constructor() { }
+  @Input() note: Note = createEmptyNote();
+
+  // @ViewChild('txtBody') txtBody!: ElementRef<HTMLInputElement>;
+  // @ViewChild('txtBody') set txtBodyRef(ref: ElementRef) {
+  //   if (!!ref){
+  //     ref.nativeElement.focus();
+  //   }
+  // }
+
+  debouncer: Subject<void> = new Subject();
+
+  constructor( private notesService: NotesService ) { }
 
   ngOnInit(): void {
+    // this.txtBodyRef.nativeElement.focus();
+    this.debouncer
+      .pipe(debounceTime(500))
+      .subscribe( () => {
+        this.update();
+      });
+  }
+
+  update() {
+    this.notesService.updateNote(this.note.id, this.note);
+  }
+
+  keyPressed() {
+    this.debouncer.next();
   }
 
 }
